67a437c31c34cfebc5e4677f5ebd9218a613ac25
Coleman Wimpy <coleman.wimpy@protonmail.com>
2024-08-15 23:41:23 -0400

    setting up nativewind/tailwind

--- ChickenTender/babel.config.js
+++ ChickenTender/babel.config.js
@@ -1,6 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
   };
 };
---
+++ ChickenTender/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.js"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
